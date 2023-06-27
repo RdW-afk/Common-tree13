596cea2495128479a5514c9fd5a11248bf91d464
RONIN808 <shubhamshedage808@gmail.com>
2023-06-27 16:21:03 +0000

    sdm660-common: Redone Display and Rendering props

--- sdm660.mk
+++ sdm660.mk
@@ -436,12 +436,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-# SurfaceFlinger
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-    ro.surface_flinger.max_virtual_display_dimension=4096 \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
-
 # Sqlite
 PRODUCT_PACKAGES += \
     sqlite3
